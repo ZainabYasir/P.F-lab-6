b236678710fdac341fc465a96a04d0741e6caf55
ZainabYasir <zaynb5637@gmail.com>
2024-10-31 14:41:48 +0500

    first commit

--- Task 05(2)/main.cpp
+++ Task 05(2)/main.cpp
@@ -4,11 +4,11 @@ int main(){
 int i=1;
 int row=5;
 while(i<=row){
-    cout<<"Row "<<i<<endl;
+    cout<<"Row "<<i<<":";
     int j=1;
     int seat=10;
     while(j<=seat){
-    cout<<"Seat "<<j<<endl;
+    cout<<"Seat "<<j<<" ";
     j++;    
     } 
     cout<<endl;
