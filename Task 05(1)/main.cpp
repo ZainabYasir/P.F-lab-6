fea82f8eaf95bdceceff7666ba31345c19eb7d97
ZainabYasir <zaynb5637@gmail.com>
2024-10-31 15:13:10 +0500

    main.cpp

--- Task 05(1)/main.cpp
+++ Task 05(1)/main.cpp
@@ -5,9 +5,9 @@ int i,j,seat,row;
 row=5;
 seat=10;
 for(i=1;i<=row;i++){
-    cout<<"Row "<<i<<endl;
+    cout<<"Row "<<i<<":";
     for(j=1;j<=seat;j++){
-    cout<<"Seat "<<j<<endl;    
+    cout<<"Seat "<<j<<" ";    
     } 
     cout<<endl;
 }
